2a0008675099ea37060d1a85c1f660f58d57b96d
Osireg17 <osireg@hotmail.com>
2022-09-26 23:14:01 +0100

    Made a migration as well as started adding users

--- server/GraphQL/resolvers.js
+++ server/GraphQL/resolvers.js
@@ -1,18 +1,32 @@
+const {User} = require('../models/');
+const bcrypt = require('bcryptjs');
+
 module.exports = { // resolvers are functions that return data for the schema
     Query: {
-        getUsers: () => {
-            const users = [
-                {
-                    username: 'John Doe',
-                    email: 'john@something.com'
-                },
-                {
-                    username: 'Jane Doe',
-                    email: 'jane@email.com'
-                }
-        
-        ];   
-        return users;
+        getUsers: async () => {
+            try {
+                const users = await User.findAll();
+                return users;
+            } catch (err) {
+                console.log(err);
+            }
+
         }
     },
-}
+
+    Mutation: {
+        register: async (_, args) => {
+            const {username, email, password, confirmPassword} = args;
+            try {
+                // TODO: Validate user data
+                const user = await User.create({
+                    username, email, password
+                });
+                
+                return user;
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        }
+    }
--- server/GraphQL/typeDefs.js
+++ server/GraphQL/typeDefs.js
@@ -8,5 +8,6 @@ type User {
     
     type Query {
     getUsers: [User]!
-}
-`;
+}, type Mutation {
+    register(username: String!, email: String!, password: String!, confirmPassword: String!): User! 
+}`
